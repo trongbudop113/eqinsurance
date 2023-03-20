3d77b3e896eb1c6ec7dc1398c08ba9b5b8767855
luuhoangtrong <trongbudop113@gmail.com>
2023-03-20 16:30:33 +0700

    commit update keystore

--- lib/configs/check_network.dart
+++ lib/configs/check_network.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 class CheckConnect{
   static Future<bool> hasNetwork() async {
     try {
-      final result = await InternetAddress.lookup('example.com');
+      final result = await InternetAddress.lookup('google.com');
       return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
     } on SocketException catch (_) {
       return false;
